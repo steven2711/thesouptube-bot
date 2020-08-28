fe6aaa0e243b9891dcbdd7a5e4402541b5bf71fc
Steven Ray Watson <swatson.emu@gmail.com>
2020-08-28 07:35:15 -0500

    Fixed time issue. Five hours behind wherever this app is hosted.

--- index.js
+++ index.js
@@ -63,24 +63,24 @@ const threeHour = 10800000;
 
 //////////////////////////////// Follow <100 people every five hours (max 400 per day) ////////////////////////////////
 
-// setInterval(() => {
-//   getStatusAndAddToFriends(getDataObject, (response) => {
-//     let filteredUsers = filterUsers(response);
+setInterval(() => {
+  getStatusAndAddToFriends(getDataObject, (response) => {
+    let filteredUsers = filterUsers(response);
 
-//     console.log(filteredUsers.length);
+    console.log(filteredUsers.length);
 
-//     let numberOfFilteredUsers = filteredUsers.length;
-//     let addInterval = numberOfFilteredUsers * 1000;
+    let numberOfFilteredUsers = filteredUsers.length;
+    let addInterval = numberOfFilteredUsers * 1000;
 
-//     for (let i = 0; i < numberOfFilteredUsers; i++) {
-//       (function (i) {
-//         setTimeout(function () {
-//           addFriend(filteredUsers[i]);
-//         }, addInterval * i);
-//       })(i);
-//     }
-//   });
-// }, fiveHour);
+    for (let i = 0; i < numberOfFilteredUsers; i++) {
+      (function (i) {
+        setTimeout(function () {
+          addFriend(filteredUsers[i]);
+        }, addInterval * i);
+      })(i);
+    }
+  });
+}, fiveHour);
 
 //////////////////////////////////// Unfollow 20 people every three hours (160 per day) //////////////////////////////////
 
@@ -104,8 +104,8 @@ const threeHour = 10800000;
 
 ///////////////////////// Post a random tweet at set interval //////////////////////////////////////////////
 
-// setInterval(() => {
-//   postRandomTweet();
-// }, thirtyMinutes);
+setInterval(() => {
+  postRandomTweet();
+}, thirtyMinutes);
 
-console.log(new Date().getHours());
+console.log(new Date().getHours() - 5);
